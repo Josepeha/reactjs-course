350c820aa5b6ec551e9962e841f6fe18cf5e7b5e
josepeha <joseperazahays@gmail.com>
2022-04-07 13:21:26 -0600

    Feat[reactjs-course]: new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import Todo from "./components/Todo";
-
 function App() {
     return (
-        <div>
-            <h1>My Todos</h1>
-            <Todo text="Learn React" />
-            <Todo text="Make Todo" />
-            <Todo text="Become as God" />
-        </div>
+        <div></div>
     )
 }
 
