2a16f003ee763dd45e8833cfc0c0d3337cc82603
josepeha <joseperazahays@gmail.com>
2022-04-11 14:52:13 -0600

    Feat[reactjs-course]: using context and states to manage favorites!

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import App from './App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import {FavoritesContextProvider} from "./stateStore/favoritesContext";
 
 const rootContainer = document.getElementById('root')
 const root = createRoot(rootContainer)
 
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <FavoritesContextProvider>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </FavoritesContextProvider>
 );
 
